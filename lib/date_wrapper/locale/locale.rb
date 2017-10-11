48bbc7d38f44de30ee86c09662378746b30217e6
Thies Pierdola <tp@centic.dk>
2017-10-11 16:44:08 +0200

    fixing paths in locale.rb

--- lib/date_wrapper/locale/locale.rb
+++ lib/date_wrapper/locale/locale.rb
@@ -1,4 +1,4 @@
 ActiveSupport.on_load(:i18n) do
-  I18n.load_path << "#{File.dirname(__FILE__)}/time_wrapper/locale/en.yml"
-  I18n.load_path << "#{File.dirname(__FILE__)}/time_wrapper/locale/da.yml"
+  I18n.load_path << "#{File.dirname(__FILE__)}/en.yml"
+  I18n.load_path << "#{File.dirname(__FILE__)}/da.yml"
 end

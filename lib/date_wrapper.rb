d889b5973fba9e771ff2ca734f2fb6b48b7ae308
Thies Pierdola <tp@centic.dk>
2017-10-12 10:35:04 +0200

    loading dependencies

--- lib/date_wrapper.rb
+++ lib/date_wrapper.rb
@@ -4,3 +4,6 @@ require "date_wrapper/simple_form_wrapper"
 require "date_wrapper/locale/locale"
 require "date_wrapper/engine"
 
+require "font-awesome-rails"
+require "bootstrap"
+require "bootstrap-datepicker-rails"
